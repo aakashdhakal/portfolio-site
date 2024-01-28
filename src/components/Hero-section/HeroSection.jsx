9966be62654276bd0d26865942a2a7b2ea31c83d
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-01-28 20:54:07 +0545

    Update image paths in HeroSection.jsx and Navbar.jsx

--- src/components/Hero-section/HeroSection.jsx
+++ src/components/Hero-section/HeroSection.jsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
         </Button>
       </div>
       <div className="hero-section-image">
-        <img src="/src/assets/illustration.svg" alt=""  />
+        <img src="/assets/illustration.svg" alt=""  />
       </div>
     </div>
   )
