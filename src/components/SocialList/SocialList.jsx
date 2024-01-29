54d925ba023711cc314935e8b1e8651e3f9a2889
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-01-29 19:25:36 +0545

    Update SocialList component to add name in Telegram link

--- src/components/SocialList/SocialList.jsx
+++ src/components/SocialList/SocialList.jsx
@@ -7,7 +7,7 @@ export default function SocialList() {
           <li><a href="#" target="_blank" ><i className="fa-brands fa-x-twitter"></i></a></li>
           <li><a href="#" target="_blank" ><i className="fa-brands fa-linkedin-in"></i></a></li>
           <li><a href="#" target="_blank" ><i className="fa-brands fa-github"></i></a></li>
-          <li><a href="#" target="_blank" ><i className="fa-brands fa-telegram"></i></a></li>
+          <li><a href="#" target="_blank" >Aakash<i className="fa-brands fa-telegram"></i></a></li>
         </ul>
     )
 }
