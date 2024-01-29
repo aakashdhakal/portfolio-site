24c0ada460e69f174e865dc560698c18e1808687
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-01-29 11:39:36 +0545

    update

--- src/components/SocialList/SocialList.jsx
+++ src/components/SocialList/SocialList.jsx
@@ -4,10 +4,10 @@ import './SocialList.css';
 export default function SocialList() {
   return (
     <ul className="social-list">
-          <li><a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-x-twitter"></i></a></li>
-          <li><a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a></li>
-          <li><a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></li>
-            <li><a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-telegram"></i></a></li>
+          <li><a href="#" target="_blank" ><i className="fa-brands fa-x-twitter"></i></a></li>
+          <li><a href="#" target="_blank" ><i className="fa-brands fa-linkedin-in"></i></a></li>
+          <li><a href="#" target="_blank" ><i className="fa-brands fa-github"></i></a></li>
+          <li><a href="#" target="_blank" ><i className="fa-brands fa-telegram"></i></a></li>
         </ul>
     )
 }
