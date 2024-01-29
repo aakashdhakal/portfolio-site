5e3c9107597d5fa2f8f8818a1052ac10e524ea9a
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-01-29 19:46:15 +0545

    Refactor social list component CSS and JSX

--- src/components/SocialList/SocialList.jsx
+++ src/components/SocialList/SocialList.jsx
@@ -3,11 +3,11 @@ import './SocialList.css';
 
 export default function SocialList() {
   return (
-    <ul className="social-list1">
+    <ul className="social-link">
           <li><a href="#" target="_blank" ><i className="fa-brands fa-x-twitter"></i></a></li>
           <li><a href="#" target="_blank" ><i className="fa-brands fa-linkedin-in"></i></a></li>
           <li><a href="#" target="_blank" ><i className="fa-brands fa-github"></i></a></li>
-          <li><a href="#" target="_blank" >Aakash<i className="fa-brands fa-telegram"></i></a></li>
+          <li><a href="#" target="_blank" ><i className="fa-brands fa-telegram"></i></a></li>
         </ul>
     )
 }
