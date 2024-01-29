fd67ff0fe3ba1631301f6e9a1b19d217d1decf00
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-01-29 19:42:29 +0545

    Update class names in SocialList.css

--- src/components/SocialList/SocialList.jsx
+++ src/components/SocialList/SocialList.jsx
@@ -3,7 +3,7 @@ import './SocialList.css';
 
 export default function SocialList() {
   return (
-    <ul className="social-list">
+    <ul className="social-list1">
           <li><a href="#" target="_blank" ><i className="fa-brands fa-x-twitter"></i></a></li>
           <li><a href="#" target="_blank" ><i className="fa-brands fa-linkedin-in"></i></a></li>
           <li><a href="#" target="_blank" ><i className="fa-brands fa-github"></i></a></li>
