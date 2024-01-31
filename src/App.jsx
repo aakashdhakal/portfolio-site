b2155bb03dcf656d92019e56b533032428b729e5
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-01-31 21:37:56 +0545

    Add AboutSection and BlogCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './style.css'
 import HeroSection from './components/Hero-section/HeroSection.jsx'
 import Navbar from './components/Navbar/Navbar.jsx';
+import AboutSection from './components/AboutSection/AboutSection.jsx'
+import BlogCard from './components/BlogCard/BlogCard.jsx'
 
 
 export default function Home() {
 
   return (
+    <main>
     <section id='navHero'>
       <Navbar />
       <div className="hero-section">
         <HeroSection />
       </div>
       </section>
+      <AboutSection />
+      <BlogCard />
+      </main>
   )
 }
 
---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,16 @@
+import './AboutSection.css'
+
+export default function AboutSection() {
+    return (
+      <section id="aboutSection">
+        <div className="about-text">
+                  <h1>About me</h1>
+
+          <p>My name is Aakash Dhakal Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum sapiente delectus facilis officia illo ea ipsam illum ducimus modi quaerat, aut debitis distinctio est! Molestias voluptates odit optio voluptatem doloremque accusantium illum nobis nemo totam, maxime impedit iusto nostrum vitae eius fuga cumque enim asperiores quae ipsam libero nesciunt omnis? Odio fugiat laborum consequuntur distinctio ducimus vero pariatur dolor cupiditate, eveniet, recusandae error corrupti illum maiores ut a quasi consectetur placeat? Earum sequi repudiandae fugit nobis ullam possimus cumque similique tenetur eaque provident, sapiente nihil sed accusamus dolorum sunt eum delectus aut voluptatibus voluptatem obcaecati eveniet, natus nam! Vero mollitia esse illo reprehenderit quibusdam eaque perspiciatis culpa nisi facere distinctio. Suscipit accusamus beatae natus, dolorum explicabo temporibus eveniet sunt adipisci dolore aliquid recusandae? In eum iste obcaecati maiores nostrum error officiis deleniti voluptate molestiae, dolorem ad quis, aliquam perferendis </p>
+        </div>
+        <div className="about-image">
+          <img src="/hero-img.png" />
+        </div>
+      </section>
+    )
+} 
