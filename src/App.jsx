fffea8a753bca946c8088f5ba2e6e2199f24a55e
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-05-23 14:05:25 +0545

    Refactor button and social list components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import './style.css'
-import HeroSection from './components/Hero-section/HeroSection.jsx'
-import Navbar from './components/Navbar/Navbar.jsx';
-import AboutSection from './components/AboutSection/AboutSection.jsx'
-import Button from './components/Buttons/button.jsx';
-import { ScrollToTop } from './components/customHooks/customHooks.jsx';
 
 
 export default function Home() {
 
   return (
-    <main>
-      <ScrollToTop />
-    <section id='navHero'>
-      <Navbar />
-      <div className="hero-section">
-        <HeroSection />
+    <div className="container">
       </div>
-      </section>
-      <AboutSection />
-      </main>
   )
 }
 
--- src/components/Buttons/button.jsx
+++
@@ -1,5 +0,0 @@
-import './button.css'
- function Button(props) {
-   return <button className={props.name} onClick={props.click}>{props.children}</button>
-}
-export default Button;
